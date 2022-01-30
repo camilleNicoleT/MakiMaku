55dec486c6bf643296c3fb5a095e11548d3e1baa
camilleNicole <camilleht@gmail.com>
2022-01-29 19:21:32 -0500

    deleted extra unneeded text

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,6 @@
 var genre = document.querySelector('#genre-input');
 var movieList = document.querySelector('#movie-list');
 
-var genre = document.querySelector('.genre-input');
-var movieList = document.querySelector('.movie-list');
-
-
 var getMovies = function(genre) {
     var apiUrl = 'https://api.themoviedb.org/3/discover/movie?api_key=1a546d3d450a8b39075fa9e7e9e4d391&language=en-US&sort_by=popularity.desc&include_adult=false&page=1&with_genres=' + genre;
     
