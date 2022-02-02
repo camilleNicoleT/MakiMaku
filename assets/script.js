5747b3c23c2632bc00ddaab9bad95f8cc02a4d38
camilleNicole <camilleht@gmail.com>
2022-02-01 23:24:21 -0500

    Button Icon and Restaurant click event

--- assets/script.js
+++ assets/script.js
@@ -3,9 +3,13 @@ var movieList = document.querySelector('.movie-list');
 var generateBtn = document.querySelector('#btn');     
 var genre;
 var page = 1;
-var zipInput = document.getElementById('zipcode-input').value;
-var zipCodeId = document.getElementById('zipcode-input').getAttribute('id')
+
+//var zipCodeKey = document.getElementById('zipcode-input').getAttribute('id')
 var genreKey = document.querySelector('span').getAttribute("id");
+
+//var zipcodeInputEl = document.getElementById("#zip-input");
+//var userFormEl = document.querySelector("#btn");
+var restaurantListEl = document.querySelector("#restaurant-list");
 
 var getMovies = function() {
 
@@ -58,25 +62,28 @@ var getMovies = function() {
 }
 
 
-// function paginated_fetch(
-//   url = is_required('https://api.themoviedb.org/3/discover/movie?api_key=1a546d3d450a8b39075fa9e7e9e4d391&language=en-US&include_adult=false'),
-//   page = 1,
-//   previousResponse = []
-// ) {
-//   return fetch(`${url}&page=${page}&with_genres=`+genre) // Append the page number to the base URL
-//     .then(response => response.json())
-//     .then(newResponse => {
-//       const response = [...previousResponse, ...newResponse]; // Combine the two arrays
+var getRestaurant = function(zipCode) {
+  var url = "https://api.documenu.com/v2/restaurants/?zip_code="+ zipCode + "key=89a322aeccb5ab89c42ab8f70808d1f9"
+  fetch(url)
+  .then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+      console.log(data)
+      displayRestaurants(data)
+      });
+    }
+  })
+}
 
-//       if (newResponse.length !== 0) {
-//         page++;
-//        return fetch(url, page, response);
-        
-//       }
-//      return response;
-    //      });
-//     console.log("responded");
+var displayRestaurants = function(restaurants){
+  for (i = 0; i < 5; i++) {
+    console.log();
+    var restaurantName = document.createElement("li")
+    restaurantName.textContent = restaurants.data[i].restaurant_name
+    restaurantListEl.append(restaurantName);
+  }
 
+}
  
 
 // function loadDateIdeas() {
@@ -93,12 +100,26 @@ var getMovies = function() {
 //   loadDateIdeas();}
  
 
+// var formSubmitHandler = function(event){ 
+//   event.preventDefault()
+//   var zipCode = zipcodeInputEl.value.trim()
+
+//   if (zipCode) {
+//     getRestaurant(zipCode);
+//     zipcodeInputEl.value = "";
+//   } else {
+//     alert("Please enter a zipcode")
+//   }
+//   console.log(zipCode)
+// };
+
+
 document.getElementById('btn').onclick = function() {
+ // event.preventDefault()
   
   var radios = document.getElementsByName('genre');
   for (var radio of radios)
-  
-  {
+    {
       if (radio.checked) {
         
         genre = radio.value;
@@ -106,19 +127,26 @@ document.getElementById('btn').onclick = function() {
         console.log(genre);
        }
       }
-  
- zipInput = document.getElementById('zipcode-input').value;
-  JSON.stringify(zipInput);
-  console.log(zipInput);
+ 
+var zipCode = document.getElementById('zip-input').value.trim();
+   JSON.stringify(zipCode);
+  console.log(zipCode);
 
-  zipCodeId = document.getElementById('zipcode-input').getAttribute('id')
- localStorage.setItem(zipCodeId, zipInput);
+ if (zipCode) {
+  getRestaurant(zipCode);
+  zipCode.value = "";
+} else {
+  alert("Please enter a zipcode")
+}
+
+  zipCodeKey = document.getElementById('zip-input').getAttribute('id')
+ localStorage.setItem(zipCodeKey, zipCode);
 
 genreKey = document.querySelector('span').getAttribute("id");
   localStorage.setItem(genreKey, genre);
-      // if(zipInput==null)
+ 
         
 
   getMovies();
-  //getRestaurant();
-  }
+  getRestaurant(zipCode);
+}
